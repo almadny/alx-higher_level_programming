86d062123b7bf89da1699d6a6d1051c14a7b911b
Almadny Nafiu <Almadny2u@gmail.com>
2022-12-26 14:45:03 +0000

    Project 12 Edited

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    while b > 0:
-        result *= a
-        b -= 1
+    result = 
+    if b < 0:
+        b *= -1
+        while b > 0:
+            result *= a
+            b -= 1
+        result = 1 / result
+    else:
+        while b > 0:
+            result *= a
+            b -= -1 
     return result

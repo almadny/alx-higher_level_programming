96cd09fa50cd36e1b8ca5b49f76474f2d8b511ca
Almadny <Almadny2u@gmail.com>
2023-01-04 03:47:44 -0800

    Pycodestyle corrections

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,54 +1,53 @@
 #!/usr/bin/python3
-""" 
-    A Python module that represents 
-
+"""
+    A Python module that represents
     A  Rectangle class with
-    width 
+    width
     and height
 """
 
+
 class Rectangle:
     """ A Class that defines a rectangle """
-    
     def __init__(self, width=0, height=0):
         """ Initializes the instance variables """
         self.width = width
         self.height = height
-    
+
     def __del__(self):
         """ Deletes a rectangle object """
         print("Bye rectangle...")
-    
+
     def __str__(self):
         """ Returns the string representation of the rectangle object """
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join("#" * self.width for i in range(self.height))
- 
+
     def __repr__(self):
-        """ Returns the string representation of a recreateable of the object """
+        """ Returns a recreateable instance of the object """
         return "Rectangle({}, {})".format(self.width, self.height)
 
     @property
     def width(self):
         """ Returns the width of the rectangle """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """ Sets the width property of the rectangle """
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if  value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-    
+
     @property
     def height(self):
         """ Returns the height of the rectangle """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """ Sets the height attribute of an instance """

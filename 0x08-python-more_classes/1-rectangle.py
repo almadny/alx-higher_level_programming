e54cb0763c40d46dfb3e629bf8d15799cfb7b7f0
Almadny <Almadny2u@gmail.com>
2023-01-04 03:25:22 -0800

    Pycodestyle corrected

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" 
-    A Python module that represents 
+"""
+    A Python module that represents
 
     A  Rectangle class with
-    width 
+    width
     and height
 """
 
 
 class Rectangle:
-    """ A Class that defines a rectangle """    
+    """ A Class that defines a rectangle """
     def __init__(self, width=0, height=0):
         """ Initializes the instance variables """
         self.width = width
@@ -25,7 +25,7 @@ class Rectangle:
         """ Sets the width property of the rectangle """
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if  value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -34,7 +34,7 @@ class Rectangle:
     def height(self):
         """ Returns the height of the rectangle """
         return self.__height
-   
+
     @height.setter
     def height(self, value):
         """ Sets the height attribute of an instance """

bc3951750d2d18bfc6bd2aafc57dd2a764570293
Almadny <Almadny2u@gmail.com>
2023-02-08 00:52:08 -0800

    Class and Object T0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ A Module with an Empty Class """
+
+
 class Square:
-        """A class that defines a square
-        This class is an empty one for now."""
-        pass
+    """A class that defines a square.
+    This class is an empty one for now."""
+    pass

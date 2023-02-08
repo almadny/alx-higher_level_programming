3dff82687a53b4b5a78086ccf2efbc77e9984dec
Almadny <Almadny2u@gmail.com>
2023-02-08 01:01:05 -0800

    T1 Added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A Module with an Empty Class """
+
+
+class Square:
+    """A class that defines a square.
+    This class is an empty one for now."""
+
+    def __init__(self, size):
+        """ Initializes a square with a size """
+        self.__size = size

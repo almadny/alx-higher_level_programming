8041dfd74d8d6a7e7f0314ca556c252af8ad3711
Almadny <Almadny2u@gmail.com>
2023-02-08 01:57:26 -0800

    T4 Added

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" A Module with an Empty Class """
+
+
+class Square:
+    """A class that defines a square.
+    This class is an empty one for now."""
+
+    def __init__(self, size=0):
+        """ Initializes a square with a size """
+
+        self.__size = size
+
+    def area(self):
+        """ Calculate the area of the square"""
+
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Set the size of the square"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value

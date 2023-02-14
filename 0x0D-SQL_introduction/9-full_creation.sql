0a06ce979ba8d30980bf60d6fc16614cea856c1c
Almadny <Almadny2u@gmail.com>
2023-02-14 07:17:10 -0800

    T9 Added

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Create a table and insert records into it
+-- Create table second_table
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+-- Insert Data into the table
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+

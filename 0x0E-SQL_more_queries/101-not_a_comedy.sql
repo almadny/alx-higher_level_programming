b143f3f889e690947ce04ee46a367625aa812154
Almadny <Almadny2u@gmail.com>
2023-02-17 06:43:11 -0800

    T101 Added

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,8 @@
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tv_shows.title
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id NOT IN (
-	SELECT tv_show_genres.genre_id
+WHERE tv_shows.id NOT IN (
+	SELECT DISTINCT(tv_show_genres.show_id)
 	FROM tv_show_genres
 	INNER JOIN tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id

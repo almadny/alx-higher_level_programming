828f3efc140f3d80883f6cc1c771c88f9b807a1a
Almadny <Almadny2u@gmail.com>
2023-02-16 18:25:52 -0800

    Amended T13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_genres.name AS name, count(*) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres
+INNER JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;

2e81f182732e23b03d6823c2b320cb79dac54f8e
Almadny <Almadny2u@gmail.com>
2023-02-20 11:19:06 -0800

    T100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,6 @@
 -- Convert Database to utf8
 -- Select the database
 USE hbtn_0c_0;
--- Convert Database
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 -- Convert Table
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
@@ -12,4 +10,5 @@ ALTER TABLE first_table CHANGE
 name name VARCHAR(256)
 CHARACTER SET utf8mb4 COLLATE
 utf8mb4_unicode_ci;
-
+-- Convert Database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;

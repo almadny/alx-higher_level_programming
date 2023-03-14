79ea6bb9ee44fa8555fb9cded0864c1e6357cdfc
Almadny <Almadny2u@gmail.com>
2023-03-14 15:59:11 +0000

    T3 Edited

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let i = 2;
-while (process.argv[i]){
-	console.log(process.argv[i]);
-	i++;
+while (process.argv[i]) {
+  console.log(process.argv[i]);
+  i++;
 }
-if (i < 3){
-	console.log('No argument');
+if (i < 3) {
+  console.log('No argument');
 }

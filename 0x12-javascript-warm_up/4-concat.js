4d8c618b3926d3db6f4f3af2c13368c073f58efe
Almadny <Almadny2u@gmail.com>
2023-03-14 16:46:29 +0000

    T4 Corrected

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-let arg1 = process.argv[2];
-let arg2 = process.argv[3];
-console.log(process.argv[2],'is', process.argv[3]);
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(arg1, 'is', arg2);

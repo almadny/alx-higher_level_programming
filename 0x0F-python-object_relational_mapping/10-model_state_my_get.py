e51dad91baea4087ed5686eb3d77dfdd1f97f573
Almadny <Almadny2u@gmail.com>
2023-03-24 08:27:58 +0000

    Task 10 Added

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A Module that defines a model for connectin Database using SQLAlchemy """
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker, Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State.id).filter(State.name == sys.argv[4])
+    if len(states.all()) == 0:
+        print('Not Found')
+    else:
+        print(states[0][0])

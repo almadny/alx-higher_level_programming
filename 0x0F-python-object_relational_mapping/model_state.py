2c431a296453597732ef5430ae82faf23c13e239
Almadny <Almadny2u@gmail.com>
2023-03-24 07:08:11 +0000

    Fix Pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,14 +4,15 @@ from sqlalchemy import create_engine, MetaData, Table, Integer, String, \
         Column, ForeignKey, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'.format('root', 'root'), pool_pre_ping=True)
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
+                       .format('root', 'root'), pool_pre_ping=True)
 Base = declarative_base()
+
 
 class State(Base):
     """ Model that defines states table in MySQL """
-    
-    
+
     __tablename__ = "states"
     id = Column(Integer(), primary_key=True)
     name = Column(String(128), nullable=False)
-

6b2c8a6c7f62a1b653d922431ba22976ca87cb3f
Almadny <Almadny2u@gmail.com>
2023-03-24 09:45:50 +0000

    Task 13 Added

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A Module that defines a model for connectin Database using SQLAlchemy """
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker, Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter(
+        State.name.like('%a%')).delete(synchronize_session='fetch')
+    session.commit()

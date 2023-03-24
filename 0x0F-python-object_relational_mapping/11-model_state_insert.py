8fa650a0999024eba04e21b7fdabd41181dd4330
Almadny <Almadny2u@gmail.com>
2023-03-24 08:56:51 +0000

    Task 11 Added

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A Module that defines a model for connectin Database using SQLAlchemy """
+import sys
+import sqlalchemy
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker, Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    s1 = State(name='Louisiana')
+    session.add(s1)
+    session.commit()
+    print(s1.id)

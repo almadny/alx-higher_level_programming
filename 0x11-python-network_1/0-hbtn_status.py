52b9d8afd3e2de61cf7c023864920dec9822ab5b
Nafiu Ahmad <Almadny2u@gmail.com>
2023-04-24 06:42:40 -0700

    Task 0 Added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """ Use urllib to send URL Resquest """
-import urllib
+import urllib.request
 if __name__ == "__main__":
-    
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as content:
+        cont = content.read()
+    print("Body response: \n\t - type: {}\n\t - content: {}\n\t
+          - utf - 8 content: {}"
+          .format(type(cont), cont, cont.decode(encoding='utf-8')))

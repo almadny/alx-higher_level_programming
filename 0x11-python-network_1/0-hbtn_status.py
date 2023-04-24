329826bbbc6effaa577857e70d8d40312b507171
Nafiu Ahmad <Almadny2u@gmail.com>
2023-04-24 07:21:46 -0700

    Added a Request object

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-Use urllib to send URL Resquest
-"""
+""" Use urllib to send URL Resquest"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as content:
+    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(request) as content:
         cont = content.read()
     print("Body response: \n\t - type: {}\n\t - content: {}\n\t
           - utf - 8 content: {}"

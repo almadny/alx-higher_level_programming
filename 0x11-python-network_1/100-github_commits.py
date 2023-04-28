cb10af85f5ef50ed82f4973d988b3248cd80e71a
Almadny <Almadny2u@gmail.com>
2023-04-28 13:01:27 +0000

    Advanced task fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,10 +8,9 @@ if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    resp = requests.get(url)
-    rp = resp.json()
+    resp = requests.get(url).json()
     for i in range(10):
         print("{}: {}".format(
-                    rp[i].get('commit').get('tree').get('sha'),
-                    rp[i].get('commit').get('author').get('name'),
+                    resp[i].get('sha'),
+                    resp[i].get('commit').get('author').get('name'),
                     ))
